2a11182b2c0ed9f8139e940304acc5b01e199540
Alesio Clement <54912841+Alesio-Clement@users.noreply.github.com>
2020-07-06 13:24:30 +0300

    starter files

---
+++ js/scripts.js
@@ -0,0 +1,181 @@
+/*!
+    * Start Bootstrap - Agency v6.0.1 (https://startbootstrap.com/template-overviews/agency)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
+    */
+
+   let modalId = $('#image-gallery');
+
+   $(document)
+     .ready(function () {
+   
+       loadGallery(true, 'a.thumbnail');
+   
+       //This function disables buttons when needed
+       function disableButtons(counter_max, counter_current) {
+         $('#show-previous-image, #show-next-image')
+           .show();
+         if (counter_max === counter_current) {
+           $('#show-next-image')
+             .hide();
+         } else if (counter_current === 1) {
+           $('#show-previous-image')
+             .hide();
+         }
+       }
+   
+       /**
+        *
+        * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+        * @param setClickAttr  Sets the attribute for the click handler.
+        */
+   
+       function loadGallery(setIDs, setClickAttr) {
+         let current_image,
+           selector,
+           counter = 0;
+   
+         $('#show-next-image, #show-previous-image')
+           .click(function () {
+             if ($(this)
+               .attr('id') === 'show-previous-image') {
+               current_image--;
+             } else {
+               current_image++;
+             }
+   
+             selector = $('[data-image-id="' + current_image + '"]');
+             updateGallery(selector);
+           });
+   
+         function updateGallery(selector) {
+           let $sel = selector;
+           current_image = $sel.data('image-id');
+           $('#image-gallery-title')
+             .text($sel.data('title'));
+           $('#image-gallery-image')
+             .attr('src', $sel.data('image'));
+           disableButtons(counter, $sel.data('image-id'));
+         }
+   
+         if (setIDs == true) {
+           $('[data-image-id]')
+             .each(function () {
+               counter++;
+               $(this)
+                 .attr('data-image-id', counter);
+             });
+         }
+         $(setClickAttr)
+           .on('click', function () {
+             updateGallery($(this));
+           });
+       }
+     });
+   
+   // build key actions
+   $(document)
+     .keydown(function (e) {
+       switch (e.which) {
+         case 37: // left
+           if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+             $('#show-previous-image')
+               .click();
+           }
+           break;
+   
+         case 39: // right
+           if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+             $('#show-next-image')
+               .click();
+           }
+           break;
+   
+         default:
+           return; // exit this handler for other keys
+       }
+       e.preventDefault(); // prevent the default action (scroll / move caret)
+     });
+
+   $(document).ready(function() {
+    var panels = $('.vote-results');
+    var panelsButton = $('.dropdown-results');
+    panels.hide();
+
+    //Click dropdown
+    panelsButton.click(function() {
+        //get data-for attribute
+        var dataFor = $(this).attr('data-for');
+        var idFor = $(dataFor);
+
+        //current button
+        var currentButton = $(this);
+        idFor.slideToggle(400, function() {
+            //Completed slidetoggle
+            if(idFor.is(':visible'))
+            {
+                currentButton.html('Hide Results');
+            }
+            else
+            {
+                currentButton.html('View Results');
+            }
+        })
+    });
+});
+
+
+
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
+
